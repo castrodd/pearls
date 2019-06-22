4a644cd38cfdd7b54e71bd6954571f9f03d1d131
Daniel Castro <castrodd@gmail.com>
2019-06-21 21:47:39 -0700

    Adding comments

--- 11/quicksort.py
+++ 11/quicksort.py
@@ -1,6 +1,9 @@
 def quicksort(array, lower, upper):
+    #Check if you have more than one element in (sub)array
     if lower < upper:
+        #Split into "less than pivot" and "greater than pivot" subarrays
         p = partition(array, lower, upper)
+        #Recursively apply algorithm to subarrays
         quicksort(array, lower, p-1)
         quicksort(array, p+1, upper)
 
@@ -9,13 +12,13 @@ def partition(array, lower, upper):
     pivot = array[upper]
     for i in range(lower, upper):
         if array[i] < pivot:
-            print("increment", array[i], middle)
             middle += 1
             array[middle], array[i] = array[i], array[middle]
     print(upper, middle)
     array[upper], array[middle+1] = array[middle+1], array[upper]
     return middle+1
 
+#Test 
 array = [11, 2, 4, 13, 5, 14, 22, 17, 23, 31]
 sorted_test = quicksort(array, 0, len(array)-1)
 print(array)
